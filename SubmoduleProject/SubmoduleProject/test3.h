ac5b7073a885b09ea6f052d00d4ce7c3705bbca1
Mariam Hakobyan <hakobyanmariam@attask.com>
2013-07-25 16:47:42 +0400

    Added test3

---
+++ SubmoduleProject/SubmoduleProject/test3.h
@@ -0,0 +1,13 @@
+//
+//  test3.h
+//  SubmoduleProject
+//
+//  Created by Mariam Hakobyan on 7/25/13.
+//  Copyright (c) 2013 Mariam Hakobyan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test3 : NSObject
+
+@end
---
+++ SubmoduleProject/SubmoduleProject/test3.m
@@ -0,0 +1,13 @@
+//
+//  test3.m
+//  SubmoduleProject
+//
+//  Created by Mariam Hakobyan on 7/25/13.
+//  Copyright (c) 2013 Mariam Hakobyan. All rights reserved.
+//
+
+#import "test3.h"
+
+@implementation test3
+
+@end
