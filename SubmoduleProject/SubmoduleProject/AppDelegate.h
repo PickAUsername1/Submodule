c297762c063ad9682448a6bd5f4383dfa31bc123
Mariam Hakobyan <hakobyanmariam@attask.com>
2013-07-25 16:26:29 +0400

    - Added "Submodule" project

---
+++ SubmoduleProject/SubmoduleProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SubmoduleProject
+//
+//  Created by Mariam Hakobyan on 7/25/13.
+//  Copyright (c) 2013 Mariam Hakobyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SubmoduleProject/SubmoduleProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SubmoduleProject
+//
+//  Created by Mariam Hakobyan on 7/25/13.
+//  Copyright (c) 2013 Mariam Hakobyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
